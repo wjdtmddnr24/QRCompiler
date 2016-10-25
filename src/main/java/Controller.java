489d060fa457bee7575bfe31926cdf0ada8f58d2
jack <wjdtmddnr24@naver.com>
2016-10-26 01:01:57 +0900

    theme can be selected

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -3,14 +3,28 @@
  */
 
 import com.jfoenix.controls.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.w3c.dom.Document;
+import sun.plugin.javascript.ocx.JSObject;
+
 
 public class Controller {
 
@@ -22,16 +36,27 @@ public class Controller {
     @FXML
     private VBox editpane;
 
+    @FXML
+    private WebView editwebview;
+
+    @FXML
+    private JFXComboBox<String> comboBox;
+
 
     @FXML
     private JFXTabPane tabpane;
 
     private JFXSnackbar snackbar;
 
-
     @FXML
     void save(ActionEvent event) {
         snackbar.show("저장되었습니다.", 2000);
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        String content = (String) clipboard.getContent(DataFormat.PLAIN_TEXT);
+        content = StringEscapeUtils.escapeEcmaScript(content);
+        String query = "editor.insert('" + content + "');";
+        System.out.print(query);
+        editwebview.getEngine().executeScript(query);
 
     }
 
@@ -68,5 +93,82 @@ public class Controller {
         snackbar.getStylesheets().add(getClass().getResource("snackbar_styles.css").toExternalForm());
         snackbar.setId("snackbar");
         snackbar.setStyle("-fx-text-fill: #FFFFFF;");
+
+        editwebview.getEngine().setJavaScriptEnabled(true);
+        editwebview.getEngine().load(getClass().getResource("src-noconflict/index.html").toExternalForm());
+        editwebview.getEngine().documentProperty().addListener(new ChangeListener<Document>() {
+            public void changed(ObservableValue<? extends Document> prop, Document oldDoc, Document newDoc) {
+                enableFirebug(editwebview.getEngine());
+            }
+        });
+
+        editwebview.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                if (event.isControlDown() && event.getCode() == KeyCode.V) {
+                    final Clipboard clipBoard = Clipboard.getSystemClipboard();
+                    String content = (String) clipBoard.getContent(DataFormat.PLAIN_TEXT);
+                    content = StringEscapeUtils.escapeEcmaScript(content);
+                    String query = "editor.insert('" + content + "');";
+                    editwebview.getEngine().executeScript(query);
+                    editwebview.getEngine().executeScript("editor.setTheme(\"ace/theme/dracula\");");
+
+                }
+            }
+        });
+
+
+        comboBox.getStylesheets().add(getClass().getResource("combobox.css").toExternalForm());
+        comboBox.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                String theme = comboBox.getSelectionModel().getSelectedItem();
+                System.out.println("editor.setTheme(\"ace/theme/" + theme + "\");");
+                editwebview.getEngine().executeScript("editor.setTheme(\"ace/theme/" + theme + "\");");
+            }
+        });
+        setComboBoxItems(comboBox);
+        comboBox.getSelectionModel().select(0);
+
+    }
+
+    private void enableFirebug(final WebEngine engine) {
+        editwebview.getEngine().executeScript("if (!document.getElementById('FirebugLite')){E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');E['setAttribute']('id', 'FirebugLite');E['setAttribute']('src', 'https://getfirebug.com/' + 'firebug-lite.js' + '#startOpened');E['setAttribute']('FirebugLite', '4');(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);E = new Image;E['setAttribute']('src', 'https://getfirebug.com/' + '#startOpened');}");
+    }
+
+    private void setComboBoxItems(ComboBox<String> comboBox) {
+        comboBox.getItems().add("ambiance");
+        comboBox.getItems().add("chaos");
+        comboBox.getItems().add("chrome");
+        comboBox.getItems().add("clouds");
+        comboBox.getItems().add("clouds_midnight");
+        comboBox.getItems().add("cobalt");
+        comboBox.getItems().add("crimson_editor");
+        comboBox.getItems().add("dawn");
+        comboBox.getItems().add("dreamweaver");
+        comboBox.getItems().add("clipse");
+        comboBox.getItems().add("github");
+        comboBox.getItems().add("idle_fingers");
+        comboBox.getItems().add("iplastic");
+        comboBox.getItems().add("katzenmilch");
+        comboBox.getItems().add("kr_theme");
+        comboBox.getItems().add("kuroir");
+        comboBox.getItems().add("merbivore");
+        comboBox.getItems().add("merbivore_soft");
+        comboBox.getItems().add("mono_industrial");
+        comboBox.getItems().add("monokai");
+        comboBox.getItems().add("pastel_on_dark");
+        comboBox.getItems().add("solarized_dark");
+        comboBox.getItems().add("solarized_light");
+        comboBox.getItems().add("sqlserver");
+        comboBox.getItems().add("terminal");
+        comboBox.getItems().add("textmate");
+        comboBox.getItems().add("tomorrow");
+        comboBox.getItems().add("tomorrow_night");
+        comboBox.getItems().add("tomorrow_night_blue");
+        comboBox.getItems().add("tomorrow_night_bright");
+        comboBox.getItems().add("tomorrow_night_eighties");
+        comboBox.getItems().add("twilight");
+        comboBox.getItems().add("vibrant_ink");
+        comboBox.getItems().add("xcode");
+
     }
 }
